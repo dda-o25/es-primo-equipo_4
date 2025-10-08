1a2c3fb3a618be67ae5549d90d58c1122529c2d7
chrisfb1901 <christian.flores@iteso.mx>
2025-10-08 08:22:34 -0600

    es_primo

--- es_primo.py
+++ es_primo.py
@@ -1,15 +1,29 @@
 """
-Inserta el encabezado aquí y escribe tu código abajo
+ANGELICA ELIZABETH IBARRA DIAZ
+DIAZARTURO ARJONA HERMOSILLOA
+CHRISTIAN ALONSO FLORES BURGOS
+MARCO FABRIZIO AZPEITIA CASTELLANOS
+
+10 de octubre 2025
+
+Determinar si un número dado es primo
 """
 
 # Declaraciones
-CONSTANTE = valor
 
-# Entradas
-entrada = input()
+#Entradas
+numero = int(input("Inserte numero:"))
+es_primo = True
+#Proceso 
+if numero <= 1:
+    es_primo = False
+else:
+    for i in range(2, numero):  
+        if numero % i == 0:     
+            es_primo = False
 
-# Proceso
-
-
-# Salidas
-print(salida)
+#Salidas
+if es_primo:
+    print(f"{numero} es un número primo.")
+else:
+    print(f"{numero} no es un número primo.")
